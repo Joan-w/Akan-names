0e1cfd9d0b2c2c0a8df5cfd676b5d2b15d5de7d1
joan-w <wamuyujoan4@gmail.com>
2020-02-16 23:00:53 +0300

    Writing variables and arrays

--- js/script.js
+++ js/script.js
@@ -3,13 +3,13 @@ function getName(){
     var month = document.getElementById("month").value;
     var dayOfMonth = document.getElementById("day").value;
     var date = new Date (`${year}-${month}-${dayOfMonth}`);
-    // var birthDay = date.getDay();
-    // var male = document.getElementById("male");
-    // var female = document.getElementById("female");
-    // var result = document.getElementById("result");
-    // var days = ["Sunday", "Monday","Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    // var maleNames = ["Kwasi", "Kwadwo", "Kwaku", "Kwabena", "Yaw", "Kofi", "Kwame"];
-    // var femaleNames = ["Akosua", "Adwoa", "Abenaa", "AKua", "Yaa", "Afua", "Ama"];
+    var birthDay = date.getDay();
+    var male = document.getElementById("male");
+    var female = document.getElementById("female");
+    var result = document.getElementById("result");
+    var days = ["Sunday", "Monday","Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var maleNames = ["Kwasi", "Kwadwo", "Kwaku", "Kwabena", "Yaw", "Kofi", "Kwame"];
+    var femaleNames = ["Akosua", "Adwoa", "Abenaa", "AKua", "Yaa", "Afua", "Ama"];
 
     // if (year == "" || year.length < 4 || year.length > 4){
     //     alert("Hey you need to enter a valid year!");
